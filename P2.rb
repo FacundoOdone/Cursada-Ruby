aa37c81249db9789384113ba08cb857e3350a4ab
Facundo Odone <facuodone1@gmail.com>
2022-10-08 10:08:41 -0300

    Ejercicio 10,11 y 13

--- P2.rb
+++ P2.rb
@@ -226,18 +226,107 @@ mixin.di_tcejbo
 
 module Countable
 
-  def count_invocations_of(sym)
+  def invocations
+      @invocations ||= Hash.new(0)
+  end
 
+  module ClassMethods
+      def count_invocations_of(sym)
+          alias_method(":original_#{sym}", sym) 
+
+          define_method "#{sym}" do
+              invocations[__method__] += 1
+              send(":original_#{sym}")
+          end
+      end
   end
 
   def invoked?(sym)
-
+      @invocations[sym] > 0
   end
 
   def invoked(sym)
+      @invocations[sym]
+  end
 
+  def self.included(base)
+      base.extend ClassMethods
   end
 
 end
 
+
+class Prueba
+
+  include Countable
+
+  def met1
+  end
+
+  def met2
+  end
+
+  count_invocations_of :met1
+end
+
+=begin
+p = Prueba.new
+
+p.met1
+p.met1
+p.met1
+p.met1
+
+puts p.invoked? :met1 #true
+puts p.invoked? :met2 #false
+
+puts p.invoked :met1 #4
+puts p.invoked :met2 #0
+
+=end
 #a
+
+
+class GenericFactory
+  def self.create(*args)
+    new(*args)
+  end
+  def initialize(*args)
+    raise NotImplementedError
+  end
+end
+
+class Persona < GenericFactory
+  def initialize(*args)
+    print("Mi nombre es #{args[0]} y mi apellido es #{args[1]}")
+  end
+end
+
+
+#p=Persona.create("Facundo","Odone")
+
+module Opposite
+  def opposite_2
+    return !self
+  end
+end
+
+
+class TrueClass
+  include Opposite
+
+end
+
+class FalseClass
+include Opposite
+end
+
+=begin
+p false.opposite_2
+# => true
+p true.opposite_2
+# => false
+p true.opposite_2.opposite_2
+# => true
+=end
+
